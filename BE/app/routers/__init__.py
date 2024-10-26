88fda1da7ee86458296727784a897cbac046fd67
Haris Ali <harisa73@gmail.com>
2024-10-27 02:00:47 +0500

    Fixed Import Errors in routers

--- BE/app/routers/__init__.py
+++ BE/app/routers/__init__.py
@@ -1,8 +1,6 @@
-from .products import products as products_router
-from .users import users as users_router
+from .products import router as products_router
 from fastapi import APIRouter
 
 router = APIRouter()
 
-router.include_router(products_router, prefix="/users", tags=["users"])
 router.include_router(products_router, prefix="/products", tags=["products"])
--- BE/app/routers/products.py
+++ BE/app/routers/products.py
@@ -8,7 +8,7 @@ from typing import List
 
 router = APIRouter()
 
-@router.get("/{product_id}", response_model=List[ProductResponse], dependencies=[Depends(get_current_user)])
+@router.get("/{product_id}", response_model=List[ProductResponse])
 async def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
   try:
     products = await products_crud.get_product_by_id(product_id, db)
@@ -18,7 +18,7 @@ async def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
     print(f"An error occurred: {str(e)}")
     raise HTTPException(status_code=500, detail=CRUDErrorMessages.FETCH_ERROR)
 
-@router.get("/{category_id}", response_model=List[ProductResponse], dependencies=[Depends(get_current_user)])
+@router.get("/{category_id}", response_model=List[ProductResponse])
 async def get_product_by_category_id(category_id: int, db: Session = Depends(get_db)):
   try:
     products = await products_crud.get_product_by_category_id(category_id, db)
@@ -28,7 +28,7 @@ async def get_product_by_category_id(category_id: int, db: Session = Depends(get
     print(f"An error occurred: {str(e)}")
     raise HTTPException(status_code=500, detail=CRUDErrorMessages.FETCH_ERROR)
 
-@router.get("/search?{category_id}&{model}&{year}&{main_category_name}", response_model=List[ProductResponse], dependencies=[Depends(get_current_user)])
+@router.get("/search?{category_id}&{model}&{year}&{main_category_name}", response_model=List[ProductResponse])
 async def get_product_by_category_id(brand: str = None, model: str = None, year: int = None, main_category_name: str = None, db: Session = Depends(get_db)):
   try:
     products = await products_crud.search_products(brand, model, year, main_category_name, db)
