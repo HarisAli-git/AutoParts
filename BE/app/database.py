e4b44336e6ca93316e7e67e28f4bcdc265c5bfdc
Haris Ali <harisa73@gmail.com>
2024-11-03 13:23:15 +0500

    Added requirements

--- BE/app/database.py
+++ BE/app/database.py
@@ -7,7 +7,14 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+CA_PEM_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "ca.pem")
+
+ssl_args = {
+    "ssl": {
+        "ca": CA_PEM_PATH
+    }
+}
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args=ssl_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
