2c38e7d2a7b8be9212489263a544292c5b4f09d9
HarisAli-git <harisa73@gmail.com>
2024-11-25 02:40:34 +0500

    Fixed Prot

--- BE/app/main.py
+++ BE/app/main.py
@@ -7,4 +7,4 @@ load_dotenv()
 PORT = os.getenv("PORT")
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=int(PORT) or 8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=int(PORT or 8000), reload=True)
